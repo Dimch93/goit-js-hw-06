d6e6aba15e27669b740e512b22a594bdbad29d3e
Dimch93 <dimch93@ukr.net>
2023-09-01 18:26:24 +0300

    task-1.2

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,13 @@
-const arrayTitle = document.querySelectorAll("#categories");
+const arrayTitle = document.querySelectorAll("h2");
 console.log(arrayTitle);
 arrayTitle.forEach((tittle) => {
   console.log("Categories:", tittle.textContent);
   console.log("Elements:", tittle.nextElementSibling.children.length);
+});
+
+const tittle = document.querySelectorAll(".item");
+console.log(tittle);
+tittle.forEach((tit) => {
+  console.log("Categories:", tit.firstElementChild.textContent);
+  console.log("Elements:", tit.lastElementChild.children.length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,42 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+// const liEl1 = document.createElement("li");
+// const liEl2 = document.createElement("li");
+// const liEl3 = document.createElement("li");
+// const liEl4 = document.createElement("li");
+// const liEl5 = document.createElement("li");
+// const liEl6 = document.createElement("li");
+
+// liEl1.textContent = "Potatoes";
+// liEl2.textContent = "Mushrooms";
+// liEl3.textContent = "Garlic";
+// liEl4.textContent = "Tomatos";
+// liEl5.textContent = "Herbs";
+// liEl6.textContent = "Condiments";
+
+// liEl1.classList.add("item");
+// liEl2.classList.add("item");
+// liEl3.classList.add("item");
+// liEl4.classList.add("item");
+// liEl5.classList.add("item");
+// liEl6.classList.add("item");
+
+// list.append(liEl1, liEl2, liEl3, liEl4, liEl5, liEl6);
+
+const arrey = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+list.append(...arrey);
